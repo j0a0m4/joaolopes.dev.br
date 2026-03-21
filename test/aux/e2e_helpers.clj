3f870c35cc2efbcdcb3630614d78be8f664ef005
João Lopes <me@joaolopes.dev.br>
2026-03-21 04:01:57 -0300

    fix(e2e): serve index.html for directory URIs; build artifact without BASE_PATH

--- test/aux/e2e_helpers.clj
+++ test/aux/e2e_helpers.clj
@@ -6,11 +6,13 @@
 (defn- static-handler [root-dir]
   (wrap-content-type
     (fn [req]
-      (let [path (str root-dir (:uri req))
-            file (java.io.File. path)]
-        (if (.isFile file)
-          (response/file-response path)
-          (response/file-response (str root-dir "/404.html")))))))
+      (let [path  (str root-dir (:uri req))
+            file  (java.io.File. path)
+            index (java.io.File. (str path "index.html"))]
+        (cond
+          (.isFile file)  (response/file-response path)
+          (.isFile index) (response/file-response (str path "index.html"))
+          :else           (response/file-response (str root-dir "/404.html")))))))
 
 (defmacro with-static-server [[dir port] & body]
   `(let [handler# (var-get (find-var 'aux.e2e-helpers/static-handler))
