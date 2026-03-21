8c51464717820ca87e294ea80aabbb78f0015b89
João Lopes <me@joaolopes.dev.br>
2026-03-21 07:04:50 -0300

    chore: gitignore .shadow-cljs build cache

--- .shadow-cljs/builds/app/dev/goog-js/shadow.module.main.append.js
+++
@@ -1,1 +0,0 @@
-["^ ","~:resource-id",["~:shadow.build.modules/append","~:main"],"~:source","\ntry { blog.client.core.init(); } catch (e) { console.error(\"An error occurred when calling (blog.client.core/init)\"); console.error(e); }","~:js","\ntry { blog.client.core.init(); } catch (e) { console.error(\"An error occurred when calling (blog.client.core/init)\"); console.error(e); }"]
